30fdea1cb9e0a662b5ae0d71658f0bb5354d09cf
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-04-30 14:01:34 +0530

    Logout Updated

--- hr-management-frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ hr-management-frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service'; 
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,12 +17,25 @@ export class NavBarComponent {
     private router: Router
   ) {}
 
-  logout(): void {
-    this.authService.logout();
-    this.router.navigate(['/home']);
-  }
-
   login(): void {
     this.router.navigate(['/login']);
+  }
+
+  logout(): void {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will be logged out of the system.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#2563eb',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, logout',
+      cancelButtonText: 'Cancel'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.authService.logout();
+        this.router.navigate(['/home']);
+      }
+    });
   }
 }
